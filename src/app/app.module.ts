ef853abb12556859958970224203346fb43cc05f
Brent Van Vosselen <brent.vanvosselen@live.be>
2017-11-30 12:35:42 +0100

    implementeren van routing + not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CitiesService } from './services/cities.service';
 import { NavigationComponent } from './navigation/navigation.component';
+import { NotFoundComponent } from'./not-found/not-found.component';
 import { ConvoysService } from './services/convoys.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    NavigationComponent
+    NavigationComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    RouterModule.forRoot([
+      {path: "dashboard", component:DashboardComponent},
+      {path: "**", component:NotFoundComponent}
+    ])
   ],
   providers: [
     CitiesService,
